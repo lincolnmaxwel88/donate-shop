ba062250122b2e34d2c903aa2f07e0fde534e00a
lincolnmaxwel88 <lincolnmaxwel@hotmail.com>
2025-03-01 02:07:32 -0300

    Increase password_hash field length to 256

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))
     is_admin = db.Column(db.Boolean, default=False)
     is_blocked = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, nullable=True, default=datetime.utcnow)
